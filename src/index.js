5357d49ae4b29574bd459391bed2bd08c4239359
li-maksim <max.mail4@yandex.ru>
2024-11-06 18:58:02 +0300

    Added function to handle errors with API requests

--- src/index.js
+++ src/index.js
@@ -4,19 +4,28 @@ import './style.css'
 const report = {}
 
 const getReport = async function(location) {
-    const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=KWEZ732TPU3Y92EY4JUZLPFH2`, {mode: 'cors'})
-    const data = await resp.json()
-    report.address = await data.address
-    report.temp = await data.currentConditions.temp
-    report.conditions = await data.currentConditions.conditions
-    report.precip = await data.currentConditions.preciptype
+    try {
+        const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=KWEZ732TPU3Y92EY4JUZLPFH2`, {mode: 'cors'})
+        const data = await resp.json()
+        report.address = await data.address
+        report.temp = await data.currentConditions.temp
+        report.conditions = await data.currentConditions.conditions
+        report.precip = await data.currentConditions.preciptype
+    } catch {
+        console.log('Oops!')
+        return Error
+    }
 }
 
 const Display = (() => {
 
     const showReport = async function(location) {
-        await getReport(location)
-        console.table(report)
+        const currentReport = await getReport(location)
+        if (currentReport == Error) {
+            console.log('Wrong location I guess')
+        } else {
+            console.table(report)
+        }
     }
 
     const input = document.querySelector('#location_input')
