13a5c4f0739be6f96ec421966eb40941fb63e041
li-maksim <max.mail4@yandex.ru>
2024-11-06 18:47:45 +0300

    Added search form
    Added form to look up location and display it's weather

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 import './style.css'
 // import { getData } from './script_modules/logic.js'
 
-const getData = async function(location) {
-    const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=KWEZ732TPU3Y92EY4JUZLPFH2`, {mode: 'cors'})
-    return resp.json()
-}
-
 const report = {}
 
 const getReport = async function(location) {
-    const data = await getData(location)
+    const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=KWEZ732TPU3Y92EY4JUZLPFH2`, {mode: 'cors'})
+    const data = await resp.json()
     report.address = await data.address
     report.temp = await data.currentConditions.temp
+    report.conditions = await data.currentConditions.conditions
+    report.precip = await data.currentConditions.preciptype
 }
 
-const display = async function(location) {
-    await getReport(location)
-    console.table(report)
-}
+const Display = (() => {
+
+    const showReport = async function(location) {
+        await getReport(location)
+        console.table(report)
+    }
+
+    const input = document.querySelector('#location_input')
+    const btn = document.querySelector('#location_btn')
+
+    const search = function(evt) {
+
+        evt.preventDefault()
+        let city = input.value
+        showReport(city)
+        input.value = ''
+
+    }
+
+    btn.addEventListener('click', search)
+
+    return {showReport}
+})()
+
+
 
 const Minsk = function() {
-    display('Minsk')
+    Display.showReport('Minsk')
 }
 
 Minsk()
