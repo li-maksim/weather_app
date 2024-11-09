dfdf4c04a2a6f0c2c89c56dd71ae462c37b753c8
li-maksim <max.mail4@yandex.ru>
2024-11-09 19:26:56 +0300

    Added function to display gifs
    Now site will also diplay relevant gifs for weather report.
    Also fixed bug where searching new location would reset
    C/F setting but buttons would stay the same.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 const report = {}
 
-const getReport = async function(location) {
+const getData = async function (location) {
 
     const locationArr = location.split('')
     locationArr[0] = locationArr[0].toString().toUpperCase()
@@ -15,13 +15,17 @@ const getReport = async function(location) {
     const locationStr = locationArr.join('')
 
     try {
-        const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationStr}?key=KWEZ732TPU3Y92EY4JUZLPFH2`, {mode: 'cors'})
+        const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationStr}?key=KWEZ732TPU3Y92EY4JUZLPFH2`, { mode: 'cors' })
         const data = await resp.json()
         report.address = await data.resolvedAddress
         report.temp = await data.currentConditions.temp
         report.conditions = await data.currentConditions.conditions
         report.precip = await data.currentConditions.preciptype
-        report.celcius = false
+        if (report.celcius === true) {
+            report.celcius = true
+        } else {
+            report.celcius = false
+        }
     } catch {
         console.log('Oops!')
         return Error
@@ -30,8 +34,8 @@ const getReport = async function(location) {
 
 const Display = (() => {
 
-    const showReport = async function(location) {
-        const currentReport = await getReport(location)
+    const getReport = async function (location) {
+        const currentReport = await getData(location)
         if (currentReport == Error) {
             console.log('Wrong location I guess')
             report.address = ''
@@ -48,12 +52,13 @@ const Display = (() => {
     const errorMsg = document.querySelector('.error_msg')
     const reportDiv = document.querySelector('.report_div')
 
-    const show = function() {
+    const showReport = function () {
 
         const addressP = document.createElement('p')
         const tempP = document.createElement('p')
         const descrP = document.createElement('p')
         const precipP = document.createElement('p')
+        const img = document.createElement('img')
 
         reportDiv.textContent = ''
         addressP.textContent = `Location: ${report.address}`
@@ -68,25 +73,54 @@ const Display = (() => {
         } else {
             precipP.textContent = `Precipitation: none`
         }
+
+        if (report.conditions.includes('cloudy') ||
+            report.conditions.includes('Overcast') ||
+            report.conditions.includes('Cloudy')) {
+            img.src = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTE2czIzcXdzdjdnNDA3ZTRjeHFmbDBpa2p2bmJ5N3BjdmR2cXhqcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yB3gwsCaymSglI1Jqt/giphy-downsized-large.gif'
+        } else if (report.conditions.includes('sunny') ||
+            report.conditions.includes('Sunny') ||
+            report.conditions.includes('sun') ||
+            report.conditions.includes('Sun')) {
+            img.src = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTQ1bWFiNTA3aHFwd2pmeWhkOG1wbzJkajU4cmZvdDJvcWhxbDh2ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HvYdoLbPqSdNu/giphy.gif'
+        } else if (report.conditions.includes('clear') ||
+            report.conditions.includes('Clear')) {
+            img.src = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMG5mMzVkdTY4a2k1cjNwcW8yMDliaXZ6YjFpNjhkdHRqZjc1b28zbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oz8xwT20ApeHadTlS/giphy-downsized-large.gif'
+        }
+        else if (report.conditions.includes('rain') ||
+            report.conditions.includes('Rain') ||
+            report.conditions.includes('rainy') ||
+            report.conditions.includes('Rainy')) {
+            img.src = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGFqamFtdWU3cGVlOGZxaG5iYTBma3Byd2xnOXFlNTk5NW9ydTR5byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l2ir9TVxVFOcDzMAzB/giphy.gif'
+        } else if (report.conditions.includes('snow') ||
+            report.conditions.includes('Snow') ||
+            report.conditions.includes('snowy') ||
+            report.conditions.includes('Snowy')) {
+            img.src = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGphNWhxMnIzbWphY2hnaThjYXphdXBpb29wZzBoaWk2cW5pZHV0byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26FL4IWDUYp0y6kTK/giphy.gif'
+        } else {
+            img.src = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2RydGtka3ZhOGtrZHp0cnZ6aDI1YXl6aXppamx0bzdpcjg1NnYyNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7fj4un4Rd4YTK/giphy.gif'
+        }
+
         reportDiv.appendChild(addressP)
         reportDiv.appendChild(tempP)
         reportDiv.appendChild(descrP)
         reportDiv.appendChild(precipP)
+        reportDiv.appendChild(img)
 
         input.value = ''
     }
 
-    const search = async function(evt) {
+    const search = async function (evt) {
         evt.preventDefault()
         let city = input.value
         const re = new RegExp("^([a-zA-Z\\-\\s+]{1,})$")
 
         if (re.test(city)) {
             errorMsg.textContent = ''
-            await showReport(city)
+            await getReport(city)
 
             if (report.address != '') {
-                show()
+                showReport()
             } else {
                 reportDiv.textContent = ''
             }
@@ -102,19 +136,19 @@ const Display = (() => {
 
     const CBtn = document.querySelector('#C')
     const FBtn = document.querySelector('#F')
-    
+
     const convert = (() => {
 
-        const toCelcius = function() {
+        const toCelcius = function () {
             report.celcius = true
-            show()
+            showReport()
             CBtn.classList.add('clicked')
             FBtn.classList.remove('clicked')
         }
 
-        const toFahrenheit = function() {
+        const toFahrenheit = function () {
             report.celcius = false
-            show()
+            showReport()
             FBtn.classList.add('clicked')
             CBtn.classList.remove('clicked')
         }
@@ -126,13 +160,5 @@ const Display = (() => {
     CBtn.addEventListener('click', convert.toCelcius)
     FBtn.addEventListener('click', convert.toFahrenheit)
 
-    return {showReport}
+    return { getReport }
 })()
-
-
-
-const Minsk = function() {
-    Display.showReport('Minsk')
-}
-
-Minsk()
